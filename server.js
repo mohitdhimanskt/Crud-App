11f3e1db4cbc3f259ab20ca398d62373c463a5bf
mohit <mohit.dhiman.sarkaghat@gmail.com>
2021-05-03 18:05:18 +0530

    Changes to server file

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
-app.listen(3000, function() {
-    console.log('listening on 3000')
-  })
+
   
   // We normally abbreviate `request` to `req` and `response` to `res`.
-app.get('/', function (req, res) {
-    // do something here
-  })
+// app.get('/', function (req, res) {
+//     // do something here
+//   })
   
   app.get('/', (req, res) => {
     res.send('Hello World')
+  })
+  app.listen(3000, function() {
+    console.log('listening on 3000')
   })
