07ae21ac677a2bdde26aece6760358b0bd5ef0ae
mohit <mohit.dhiman.sarkaghat@gmail.com>
2021-05-03 19:06:01 +0530

    Add to bodyparser

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-  
-  // We normally abbreviate `request` to `req` and `response` to `res`.
+// Make sure you place body-parser before your CRUD handlers!
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// We normally abbreviate `request` to `req` and `response` to `res`.
 // app.get('/', function (req, res) {
 //     // do something here
 //   })
-  
+
 //   app.get('/', (req, res) => {
 //     res.send('Hello World')
 //   })
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-    // Note: __dirname is the current directory you're in. Try logging it and see what you get!
-    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
-  })
-  app.listen(3000, function() {
-    console.log('listening on 3000')
-  })
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+  // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+});
+app.post("/quotes", (req, res) => {
+  console.log(req.body);
+});
+app.listen(3000, function() {
+  console.log("listening on 3000");
+});
