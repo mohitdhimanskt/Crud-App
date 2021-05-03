10b27c4b43b265c28bc39c3f65719686fd88fd41
mohit <mohit.dhiman.sarkaghat@gmail.com>
2021-05-03 18:08:24 +0530

    Create a new file

--- server.js
+++ server.js
@@ -7,8 +7,13 @@ const app = express();
 //     // do something here
 //   })
   
-  app.get('/', (req, res) => {
-    res.send('Hello World')
+//   app.get('/', (req, res) => {
+//     res.send('Hello World')
+//   })
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+    // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
   })
   app.listen(3000, function() {
     console.log('listening on 3000')
