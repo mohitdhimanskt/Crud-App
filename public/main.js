4dd4fe5a97e2cd384de85fff0a4b893a36f76549
mohit <mohit.dhiman.sarkaghat@gmail.com>
2021-05-06 15:22:25 +0530

    Add to JSON in main file

--- public/main.js
+++ public/main.js
@@ -3,5 +3,12 @@ const update = document.querySelector("#update-button");
 
 update.addEventListener("click", (_) => {
   // Send PUT Request here
-  fetch('/quotes', {/* ... */})
+  fetch("/quotes", {
+    method: "put",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name: "Darth Vadar",
+      quote: "I find your lack of faith disturbing.",
+    }),
+  });
 });
